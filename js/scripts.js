f1d01f55be411678c256df05e976ac5ab2d6d6ac
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-11-28 19:54:07 +0100

    start

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,61 @@
 const rootStyles = document.documentElement.style;
+const textInputElement = document.getElementById('text-input');
+const checkboxElement = document.getElementById('checkbox');
+const selectElement = document.getElementById('order');
+
+const products = [
+  {
+    image: './assets/images/image-waffle.jpg',
+    name: 'Waffle with Berries',
+    sugarless: 'yes',
+    price: '$6.50',
+  },
+  {
+    image: './assets/images/image-waffle.jpg',
+    name: 'Waffle with Berries',
+    sugarless: 'yes',
+    price: '$6.50',
+  },
+  {
+    image: './assets/images/image-waffle.jpg',
+    name: 'Waffle with Berries',
+    sugarless: 'yes',
+    price: '$6.50',
+  },
+  {
+    image: './assets/images/image-waffle.jpg',
+    name: 'Waffle with Berries',
+    sugarless: 'yes',
+    price: '$6.50',
+  },
+  {
+    image: './assets/images/image-waffle.jpg',
+    name: 'Waffle with Berries',
+    sugarless: 'yes',
+    price: '$6.50',
+  },
+  {
+    image: './assets/images/image-waffle.jpg',
+    name: 'Waffle with Berries',
+    sugarless: 'yes',
+    price: '$6.50',
+  },
+  {
+    image: './assets/images/image-waffle.jpg',
+    name: 'Waffle with Berries',
+    sugarless: 'yes',
+    price: '$6.50',
+  },
+  {
+    image: './assets/images/image-waffle.jpg',
+    name: 'Waffle with Berries',
+    sugarless: 'yes',
+    price: '$6.50',
+  },
+  {
+    image: './assets/images/image-waffle.jpg',
+    name: 'Waffle with Berries',
+    sugarless: 'yes',
+    price: '$6.50',
+  },
+];
